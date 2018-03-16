68a1dc641bcbf0a2af101e9cfbb9530802cc55a3
Valentin Bartier <val.bartier@laposte.net>
2018-03-16 10:51:17 +0100

    Add tests file for benders EE

---
+++ RobustKEPSolver/Script tests/Benders/EE/50 Scenarios/Submit/Submit-V-50-0.2-Said-6-B-E-50.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --job-name=6-0.2BE
+#SBATCH --time=03:00:00
+#
+#SBATCH --output=Out-V-B-EE-50-0.2-3-1-50-Said-6.txt
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=8192
+#SBATCH --partition=Def
+#
+#SBATCH --mail-user=bart.smeulders@ulg.ac
+#SBATCH --mail-type=ALL
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+./RobustKEP Config-V-50-0.2-Said-6-B-E-50.txt
---
+++ RobustKEPSolver/Script tests/Benders/EE/50 Scenarios/Submit/Submit-V-50-0.4-Said-10-B-E-50.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --job-name=10-0.4BE
+#SBATCH --time=03:00:00
+#
+#SBATCH --output=Out-V-B-EE-50-0.4-3-1-50-Said-10.txt
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=8192
+#SBATCH --partition=Def
+#
+#SBATCH --mail-user=bart.smeulders@ulg.ac
+#SBATCH --mail-type=ALL
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+./RobustKEP Config-V-50-0.4-Said-10-B-E-50.txt
